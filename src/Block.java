56326aef66cc28e9b6e591f4a0ad655383e44665
bradley-david <davidbradley@gmx.us>
2017-05-17 11:32:02 -0500

    Added block class

---
+++ src/Block.java
@@ -0,0 +1,13 @@
+/**
+ * Created by s641173 on 5/17/2017.
+ */
+
+
+public class Block {
+    char[][] block;
+    int[] pos;
+    public Block(int[] p, char[][] b) {
+        block = b;
+        pos = p;
+    }
+}
